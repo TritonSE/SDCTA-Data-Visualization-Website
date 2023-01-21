81c8fd8e727998d43348ada9f6e9776d50060128
James Luo <james.luo@umbocv.com>
2023-01-21 00:27:25 -0800

    Added routes and models for user and tier for mongoDB backend

--- server/index.js
+++ server/index.js
@@ -1,6 +1,19 @@
+require('dotenv').config();
+
 const express = require('express');
 const mongoose = require('mongoose');
+const mongoString = process.env.DATABASE_URL;
 
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+    console.log(error)
+})
+
+database.once('connected', () => {
+    console.log('Database Connected');
+})
 const app = express();
 
 app.use(express.json());
@@ -8,3 +21,10 @@ app.use(express.json());
 app.listen(3000, () => {
     console.log(`Server Started at ${3000}`)
 })
+
+const routesTier = require('./routes/tier');
+const routesUser = require('./routes/user');
+
+
+app.use('/api/tier', routesTier)
+app.use('/api/user', routesUser)
