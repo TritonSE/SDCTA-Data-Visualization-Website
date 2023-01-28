42bce46579944b7e6f2983338e854ba9420cb0c3
Victor Siu <siuleungjing@gmail.com>
2023-01-28 11:40:39 -0800

    Update README + index

--- server/index.js
+++ server/index.js
@@ -15,21 +15,21 @@ database.once('connected', () => {
     console.log('Database Connected');
 })
 const app = express();
-
-app.use(express.json());
-
-app.listen(3000, () => {
-    console.log(`Server Started at ${3000}`)
-})
-
 const routesTier = require('./routes/tier');
 const routesUser = require('./routes/user');
 const routesCategory = require('./routes/category');
 const routesVisualization = require('./routes/visualization');
 
-
+app.use(express.json());
 app.use('/api/tier', routesTier)
 app.use('/api/user', routesUser)
 app.use('/api/category', routesCategory)
 app.use('/api/visualization', routesVisualization)
+
+app.listen(3000, () => {
+    console.log(`Server Started at ${3000}`)
+})
+
+
+
 
