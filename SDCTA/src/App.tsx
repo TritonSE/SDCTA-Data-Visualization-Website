c68136c9b41dc8132703e32831a60f46ae97af56
Anthonyp0329 <anthonyp0329@gmail.com>
2023-02-08 17:11:35 -0800

    Squashed commit of the following:
    commit 3f342fd21adfb6e24682779014fb1c8064e6c7a7
    Author: Samvrit Srinath <114899512+SamvritSrinath@users.noreply.github.com>
    Date:   Tue Feb 7 22:18:50 2023 -0800
    
        Feature/SamvritSrinath/Landing-Page (#10)
    
        * Basic React for Landing Page
    
        Used JSX(will return to EM for feedback)
    
        * modified:   ../.DS_Store
        	modified:   package-lock.json
        	modified:   package.json
        	modified:   src/App.jsx
        	modified:   src/components/Body.jsx
        	modified:   src/components/NavBar.jsx
        	src/Images/
    
        * new file:   SDCTA/src/Images/Graph.jpg
        	new file:   SDCTA/src/Images/SDTEF Logo Transparent Background 1.jpg
        	new file:   SDCTA/src/Images/unsplash_dsV4yvL5UJk.jpg
    
        * changed jsx to tsx
    
        * added separate css file for body component
    
        * modified:   src/App.css
        	modified:   src/App.tsx
        	new file:   src/components/Footer.css
        	modified:   src/components/Footer.tsx
        	deleted:    src/components/NavBar.jsx
    
        * Images in Body
    
        * Added Education Images
        	modified:   src/components/Body.tsx
    
        * changed heading
    
        * Updating Footer Style
        	modified:   src/components/Footer.css
    
        * Updating Footer Style again
        	modified:   src/components/Footer.css
    
        * Fixing white space
        	modified:   src/components/Footer.css
        	modified:   src/components/body.css
        	new file:   src/pages/LandingPage.tsx
    
        * put in new images & added image styling
    
        * Formatting into a page
        	modified:   src/App.tsx
        	modified:   src/pages/LandingPage.tsx
    
        * Image Refactor
        	modified:   src/App.css
        	deleted:    src/Images/unsplash_dsV4yvL5UJk.jpg
        	deleted:    src/components/Body.tsx
        	deleted:    src/components/body.css
        	modified:   src/pages/LandingPage.tsx
    
        * Landing Page_Body Refactor
    
        * Images
        	renamed:    src/components/Landing Page/Body.tsx -> src/components/Landing_Page/Body.tsx
        	renamed:    src/components/Landing Page/body.css -> src/components/Landing_Page/body.css
        	modified:   src/pages/LandingPage.tsx
    
        * Final Images
        	renamed:    src/components/Footer.css -> src/components/Footer/Footer.css
        	renamed:    src/components/Footer.tsx -> src/components/Footer/Footer.tsx
        	modified:   src/components/Landing_Page/Body.tsx
        	modified:   src/pages/LandingPage.tsx
    
        ---------
    
        Co-authored-by: meganleongg <98578642+meganleongg@users.noreply.github.com>

--- SDCTA/src/App.tsx
+++ SDCTA/src/App.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+
+import {LandingPage} from './pages/LandingPage';
+
 import './App.css';
 import LogInPage from './components/auth/LogInPage'
 import SignUpPage from './components/auth/SignUpPage'
 
-function App() {
+const App = () => {
   return (
-    <div>
-      {/* <LogInPage/> */}
-      <SignUpPage/>
-    </div>
+   <div className="App">
+	<LandingPage />
+	</div>
   );
 }
+
 
 export default App;
