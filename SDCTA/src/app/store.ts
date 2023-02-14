2b328e3528fd53cdb200e2f8690acda5a5334ef9
NirmalAgnihotri <53363382+NirmalAgnihotri@users.noreply.github.com>
2023-02-14 09:46:26 -0800

    added sagas middleware

--- SDCTA/src/app/store.ts
+++ SDCTA/src/app/store.ts
@@ -1,11 +1,21 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import createSagaMiddleware from 'redux-saga'
+
+// Create the saga middleware
+const sagaMiddleware = createSagaMiddleware()
+const middleware = [sagaMiddleware]
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+  getDefaultMiddleware().concat(middleware),
 });
+
+// Then run the saga
+// sagaMiddleware.run(mySaga)
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
