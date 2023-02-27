308c792029f9f17407cc7e5595ec2e5fa5328871
NirmalAgnihotri <53363382+NirmalAgnihotri@users.noreply.github.com>
2023-02-26 17:33:08 -0800

    Fix type definition linting

--- SDCTA/src/components/modal/subscribe.tsx
+++ SDCTA/src/components/modal/subscribe.tsx
@@ -14,8 +14,8 @@ import {
 } from "./styleObjects/modals";
 
 interface ModalProps {
-  show: boolean;
-  setShow: (show: boolean) => void;
+  show: boolean
+  setShow: (show: boolean) => void
 }
 
 export const SubscribeModal: React.FC<ModalProps> = (props: ModalProps) => {
