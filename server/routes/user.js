4b10b39b656a163ff45b632505b30beeece95fa6
NirmalAgnihotri <53363382+NirmalAgnihotri@users.noreply.github.com>
2023-03-02 23:23:46 -0800

    Linted and formatted backend files

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,77 +1,70 @@
-const express = require('express');
+const express = require("express");
 
-const router = express.Router()
+const router = express.Router();
 
 module.exports = router;
 
-const UserModel = require('../models/user');
-const tierServices = require('../services/tier');
+const UserModel = require("../models/user");
+const tierServices = require("../services/tier");
 
 //Post Method
-router.post('/create', async (req, res) => {
-    try {
-        const tier = await tierServices.getTier(req.body.tier);
-        const data = new UserModel({
-            username: req.body.username,
-            email: req.body.email,
-            tier: tier._id
-        })
-        console.log(data);
-        const dataToSave = await data.save();
-        res.status(200).json(dataToSave)
-    }
-    catch (error) {
-        res.status(400).json({message: error.message})
-    }
-})
+router.post("/create", async (req, res) => {
+  try {
+    const tier = await tierServices.getTier(req.body.tier);
+    const data = new UserModel({
+      username: req.body.username,
+      email: req.body.email,
+      tier: tier._id,
+    });
+    console.log(data);
+    const dataToSave = await data.save();
+    res.status(200).json(dataToSave);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
 
 //Get all Method
-router.get('/getAll', async (req, res) => {
-    try{
-        const data = await UserModel.find();
-        res.json(data)
-    }
-    catch(error){
-        res.status(500).json({message: error.message})
-    }
-})
+router.get("/getAll", async (req, res) => {
+  try {
+    const data = await UserModel.find();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 //Get by ID Method
-router.get('/getOne/:id', async (req, res) => {
-    try{
-        const data = await Model.findById(req.params.id);
-        res.json(data)
-    }
-    catch(error){
-        res.status(500).json({message: error.message})
-    }
-})
+router.get("/getOne/:id", async (req, res) => {
+  try {
+    const data = await Model.findById(req.params.id);
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 //Update by ID Method
-router.patch('/update/:id', async (req, res) => {
-    try {
-        const id = req.params.id;
-        const updatedData = req.body;
-        const options = { new: true };
+router.patch("/update/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const updatedData = req.body;
+    const options = { new: true };
 
-        const result = await Model.findByIdAndUpdate(
-            id, updatedData, options
-        )
+    const result = await Model.findByIdAndUpdate(id, updatedData, options);
 
-        res.send(result)
-    }
-    catch (error) {
-        res.status(400).json({ message: error.message })
-    }
-})
+    res.send(result);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
 
 //Delete by ID Method
-router.delete('/delete/:id', async (req, res) => {
-    try {
-        const id = req.params.id;
-        const data = await Model.findByIdAndDelete(id)
-        res.send(`Document with ${data.username} has been deleted..`)
-    }
-    catch (error) {
-        res.status(400).json({ message: error.message })
-    }
-})
+router.delete("/delete/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const data = await Model.findByIdAndDelete(id);
+    res.send(`Document with ${data.username} has been deleted..`);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
--- server/services/tier.js
+++ server/services/tier.js
@@ -1,15 +1,14 @@
-const Model = require('../models/tier');
+const Model = require("../models/tier");
 
 module.exports = {
-    getTier: async function getTier(tier) {
-        //Get by ID Method
-        try{
-            const data = await Model.find({level: tier});
-            return data[0];
-        }
-        catch(error){
-            res.status(500).json({message: error.message})
-            return {};
-        }
+  getTier: async function getTier(tier) {
+    //Get by ID Method
+    try {
+      const data = await Model.find({ level: tier });
+      return data[0];
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+      return {};
     }
+  },
 };
