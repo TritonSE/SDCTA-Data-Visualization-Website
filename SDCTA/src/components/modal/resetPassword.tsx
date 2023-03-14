a0881445ab646302d9ea6757467186fa6abe4400
Anthonyp0329 <anthonyp0329@gmail.com>
2023-03-13 18:12:16 -0700

    updating branch

--- SDCTA/src/components/modal/resetPassword.tsx
+++ SDCTA/src/components/modal/resetPassword.tsx
@@ -1,23 +1,20 @@
 import * as React from "react";
 import { useState } from "react";
 import "../auth/auth.css";
+import "./styleObjects/resetPassword.css";
 import {
   sendPasswordResetEmail,
 } from "firebase/auth";
 
 import { auth } from "../../firebase-config";
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
 import IconButton from "@mui/material/IconButton";
 import CloseIcon from "@mui/icons-material/Close";
 
-import { style, textStyle } from "./styleObjects/signupStyle";
+import { style } from "./styleObjects/signupStyle";
 import {
-  exitButtonStyle,
-  titleStyle,
-  buttonStyle,
+  exitButtonStyle
 } from "./styleObjects/modals";
 
 interface ModalProps {
@@ -62,11 +59,14 @@ export const ResetPasswordModal: React.FC<ModalProps> = (props: ModalProps) => {
           >
             <CloseIcon />
           </IconButton>
-          <h1 className="signup-form-title">Forgot Password?</h1>
+          <h1 className="reset-title">Forgot Password?</h1>
 
-          <div className="signup-form">
-            <div className="input-boxes"></div>
+          <div className="signup-form reset-form">
             <div className="input-boxes">
+              <h3 className="reset-desc">
+                Please enter your email below, and you will be
+                emailed a link on how to reset your password.
+              </h3>
               <h3 className="textbox-label">Email</h3>
               <input
                   type="email"
@@ -75,18 +75,18 @@ export const ResetPasswordModal: React.FC<ModalProps> = (props: ModalProps) => {
                     setResetEmail(event.target.value);
                   }}
               />
+              {resetMessage !== ""
+                ? (
+                  <p>{resetMessage}</p>
+                  )
+                : (
+                    ""
+                  )}
+              <button onClick={sendReset} className="btn-signup">
+                Request Password
+              </button>
             </div>
-            {resetMessage !== ""
-              ? (
-            <p>{resetMessage}</p>
-                )
-              : (
-                  ""
-                )}
           </div>
-          <button onClick={sendReset} className="btn-signup">
-            Request Password
-          </button>
         </Box>
       </Modal>
     </div>
