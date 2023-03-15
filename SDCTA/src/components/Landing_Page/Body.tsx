c9301553ed04d7360a499d2e1ccceab4f3761940
meganleongg <98578642+meganleongg@users.noreply.github.com>
2023-03-15 02:41:04 -0700

    css styling for landing page

--- SDCTA/src/components/Landing_Page/Body.tsx
+++ SDCTA/src/components/Landing_Page/Body.tsx
@@ -1,55 +1,59 @@
-import landscape from "./../../Images/Landing_Image.jpg";
-import "./body.css";
-import EducationVis from "./../../Images/Education_Standin.png";
-import HomelessnessVis from "./../../Images/Homelessness_Standin.png";
-import MunicipalVis from "./../../Images/Municipal_Standin.png";
+import landscape from './../../Images/Landing_Image.jpg';
+import './body.css'
+import EducationVis from './../../Images/Education_Standin.png';
+import HomelessnessVis from './../../Images/Homelessness_Standin.png';
+import MunicipalVis from './../../Images/Municipal_Standin.png'
+
 
 export const Body: React.FC = () => {
-  return (
-    <>
-      <div>
-        <div className="heading">SDTEF Data Museum</div>
-        <div className="description">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, sed do eiusmod tempor incididunt ut labore et dolore
-          magna aliqua. Ut enim ad minim veniam, sed do eiusmod tempor
-          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-        </div>
-        <img src={landscape} alt="sd landscape" />
-      </div>
-      <div className="image_line">
-        <div className="first_Visual">
-          <div className="img">
-            <img src={EducationVis} alt="Education Visualization"></img>
-          </div>
-          <div className="image_line-heading">Education</div>
-          <div className="image_line-description">
-            Lorem ipsum dolor sit amet, sed do consectetur adipiscing elit, sed
-            do eiusmod tempor incididunt ut labore et
-          </div>
-        </div>
-        <div className="second_Visual">
-          <div className="img">
-            <img src={HomelessnessVis} alt="Homelessness Visualization"></img>
-          </div>
-          <div className="image_line-heading">Homelessness</div>
-          <div className="image_line-description">
-            Lorem ipsum dolor sit amet, sed do consectetur adipiscing elit, sed
-            do eiusmod tempor incididunt ut labore et
-          </div>
-        </div>
-        <div className="third_Visual">
-          <div className="img">
-            <img src={MunicipalVis} alt="Municipal Visualization"></img>
-          </div>
-          <div className="image_line-heading">Municipal</div>
-          <div className="image_line-description">
-            Lorem ipsum dolor sit amet, sed do consectetur adipiscing elit, sed
-            do eiusmod tempor incididunt ut labore et
-          </div>
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div>
+				<div className="heading" >
+					SDTEF Data Museum
+				</div>
+				<div className="description" >
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				</div>
+				<div className="imgbox">
+               		<img className="center-fit" src={landscape} alt="sd landscape" />
+            	</div>
+			</div>
+				<div className="image_line" >
+					<div className="first_Visual">
+						<div className='img' >
+							<img src={EducationVis} alt="Education Visualization" ></img>
+							</div>
+						<div className="image_line-heading" >
+							Education
+						</div>
+						<div className="image_line-description" >
+							Lorem ipsum dolor sit amet, sed do consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+						</div>
+					</div>
+					<div className="second_Visual">
+						<div className='img' >
+							<img src={HomelessnessVis} alt="Homelessness Visualization" ></img>
+							</div>
+						<div className="image_line-heading" >
+							Homelessness
+						</div>
+						<div className="image_line-description" >
+							Lorem ipsum dolor sit amet, sed do consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+						</div>
+					</div>
+					<div className="third_Visual">
+					<div className='img' >
+							<img src={MunicipalVis} alt="Municipal Visualization" ></img>
+							</div>
+						<div className="image_line-heading" >
+							Municipal
+						</div>
+						<div className="image_line-description" >
+							Lorem ipsum dolor sit amet, sed do consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+						</div>
+					</div>
+				</div> 
+			</>
+		)
 };
