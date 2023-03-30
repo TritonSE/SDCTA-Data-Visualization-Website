479e91c5aa56da80501048a5e72669dcca264b0c
Helen <59126064+Miyuki-L@users.noreply.github.com>
2023-03-30 14:25:09 -0700

    Feature/miyuki l/individual visualization page (#26)
    * Created the Individual visualization Page
    
    Co-authored-by: Brenton Dunn <brentonmdunn@users.noreply.github.com>
    
    * Fixed the styling for the page
    
    * Add csv link + new endpoint for category
    
    * Merged Branch with origin/Feature/samvritsrinath/Category-Page
    
    Co-authored-by: Brenton Dunn <brentonmdunn@users.noreply.github.com>
    
    * Embed Tableau and changed to use props (IndivVis and TableauEmbed)
    
    Co-Authored-By: Brenton Dunn <47510212+brentonmdunn@users.noreply.github.com>
    
    * Squashed commit of the following: Main
    
    Merged Branch with Main
    
    Co-Authored-By: Brenton Dunn <47510212+brentonmdunn@users.noreply.github.com>
    
    * Added visualization service and get viz by title
    
    * renamed git viz by name
    
    * Make category route return actual visualizations, instead of titles
    
    * Changed to use export arrow function instead of default
    
    Co-authored-by: Brenton Dunn <brentonmdunn@users.noreply.github.com>
    
    * Fixed Up IndivVis Page Styling.
    
    Co-Authored-By: Brenton Dunn <47510212+brentonmdunn@users.noreply.github.com>
    
    * Refactor codebase to use import + prettier
    
    * Fix bugs + continue refactoring errors
    
    * Fix eslint + address pr review
    
    * Fix prettier check
    
    * Add service layer for visualizations
    
    * Add tier services
    
    * Fix prettier
    
    * Changed cursor to pointer when hovering over signup button
    Co-authored-by: Brenton Dunn <brentonmdunn@users.noreply.github.com>
    
    * fixed lint issues and lint ignore TableauEmbed
    
    * Squashed commit of Merge with Main
    
    commit e60f8c19ff3967bb6e36b1a18c6a6beeb9119319
    Author: NirmalAgnihotri <53363382+NirmalAgnihotri@users.noreply.github.com>
    Date:   Mon Mar 13 10:54:40 2023 -0700
    
        Create README.md (#25)
    
        * Create README.md
    
        * Update README.md
    
        * Update README.md
    
    commit 63b634d48db8644ab7e236a93c6fc859b227038e
    Author: William Wu <74469686+wllmwu@users.noreply.github.com>
    Date:   Fri Mar 10 07:56:24 2023 -0800
    
        Update CODEOWNERS
    
    commit 7a975de437b35662a73cc06718534e168405dd77
    Author: Victor Siu <siuleungjing@gmail.com>
    Date:   Thu Mar 9 12:07:46 2023 -0800
    
        Feature/vs2961/backend category (#22)
    
        * Add csv link + new endpoint for category
    
        * Added visualization service and get viz by title
    
        * renamed git viz by name
    
        * Make category route return actual visualizations, instead of titles
    
        * Refactor codebase to use import + prettier
    
        * Fix bugs + continue refactoring errors
    
        * Fix eslint + address pr review
    
        * Fix prettier check
    
        * Add service layer for visualizations
    
        * Add tier services
    
        * Fix prettier
    
        * Fix bugs
    
        * Change getVisualization to name
    
        ---------
    
        Co-authored-by: James Luo <jaluo@ucsd.edu>
    Co-authored-by: Brenton Dunn <brentonmdunn@users.noreply.github.com>
    
    * Fixed loginslice and subscribeslice Lint errors
    
    * Fixed TableauEmbed Lint errors.
    
    * Removed Unecessary imports from municipal
    
    * Delete .DS_Store
    
    ---------
    
    Co-authored-by: Brenton Dunn <brentonmdunn@users.noreply.github.com>
    Co-authored-by: Victor Siu <siuleungjing@gmail.com>
    Co-authored-by: Brenton Dunn <47510212+brentonmdunn@users.noreply.github.com>
    Co-authored-by: James Luo <jaluo@ucsd.edu>
    Co-authored-by: NirmalAgnihotri <53363382+NirmalAgnihotri@users.noreply.github.com>

---
+++ SDCTA/src/pages/IndividualVisualization.tsx
@@ -0,0 +1,63 @@
+import backIcon from "./backIcon.svg";
+import downloadIcon from "./downloadIcon.svg";
+import "./IndividualVisualization.css";
+
+import { TableauEmbed } from "../components/TableauEmbed";
+
+interface IndividualVisualizationProp {
+  url: string;
+  csvlink: string;
+}
+
+export const IndividualVisualization: React.FC<IndividualVisualizationProp> = ({
+  url,
+  csvlink,
+}: IndividualVisualizationProp) => {
+  return (
+    <div>
+      <div>
+        <p className="IV-back-button">
+          {" "}
+          <img
+            style={{ paddingLeft: "3px" }}
+            src={backIcon}
+            alt="back arrow icon"
+          />{" "}
+          Back to Education Data
+        </p>
+      </div>
+      <div className="body">
+        <div className="IV-Top-row">
+          <h2>Data Subcategory Title</h2>
+          <p className="download-button">
+            Download <img src={downloadIcon} alt="download icon" />
+          </p>
+        </div>
+
+        <TableauEmbed url={url} />
+        <div className="body-text">
+          <h2 id="Analysis">Data Analysis</h2>
+          <p className="IV-description">
+            <span>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, sed do eiusmod tempor incididunt ut labore
+              et dolore magna aliqua. Ut enim ad minim veniam, sed. Lorem ipsum
+              dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua.
+            </span>
+
+            <span>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, sed do eiusmod tempor incididunt ut labore
+              et dolore magna aliqua. Ut enim ad minim veniam, sed. Lorem ipsum
+              dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna.
+            </span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
