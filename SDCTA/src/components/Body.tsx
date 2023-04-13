35ec8781f79d589c0d1b8a6947811469cb6f4f85
Helen <59126064+Miyuki-L@users.noreply.github.com>
2023-04-12 19:50:17 -0700

    Squashed commit of the following:
    commit 76898616db387a1d121c6b43a483933213fb077f
    Author: meganleongg <98578642+meganleongg@users.noreply.github.com>
    Date:   Fri Apr 7 16:05:47 2023 -0700
    
        Fixed styling for landing page (#28)
    
    commit 479e91c5aa56da80501048a5e72669dcca264b0c
    Author: Helen <59126064+Miyuki-L@users.noreply.github.com>
    Date:   Thu Mar 30 14:25:09 2023 -0700
    
        Feature/miyuki l/individual visualization page (#26)
    
        * Created the Individual visualization Page
    
        Co-authored-by: Brenton Dunn <brentonmdunn@users.noreply.github.com>
    
        * Fixed the styling for the page
    
        * Add csv link + new endpoint for category
    
        * Merged Branch with origin/Feature/samvritsrinath/Category-Page
    
        Co-authored-by: Brenton Dunn <brentonmdunn@users.noreply.github.com>
    
        * Embed Tableau and changed to use props (IndivVis and TableauEmbed)
    
        Co-Authored-By: Brenton Dunn <47510212+brentonmdunn@users.noreply.github.com>
    
        * Squashed commit of the following: Main
    
        Merged Branch with Main
    
        Co-Authored-By: Brenton Dunn <47510212+brentonmdunn@users.noreply.github.com>
    
        * Added visualization service and get viz by title
    
        * renamed git viz by name
    
        * Make category route return actual visualizations, instead of titles
    
        * Changed to use export arrow function instead of default
    
        Co-authored-by: Brenton Dunn <brentonmdunn@users.noreply.github.com>
    
        * Fixed Up IndivVis Page Styling.
    
        Co-Authored-By: Brenton Dunn <47510212+brentonmdunn@users.noreply.github.com>
    
        * Refactor codebase to use import + prettier
    
        * Fix bugs + continue refactoring errors
    
        * Fix eslint + address pr review
    
        * Fix prettier check
    
        * Add service layer for visualizations
    
        * Add tier services
    
        * Fix prettier
    
        * Changed cursor to pointer when hovering over signup button
        Co-authored-by: Brenton Dunn <brentonmdunn@users.noreply.github.com>
    
        * fixed lint issues and lint ignore TableauEmbed
    
        * Squashed commit of Merge with Main
    
        commit e60f8c19ff3967bb6e36b1a18c6a6beeb9119319
        Author: NirmalAgnihotri <53363382+NirmalAgnihotri@users.noreply.github.com>
        Date:   Mon Mar 13 10:54:40 2023 -0700
    
            Create README.md (#25)
    
            * Create README.md
    
            * Update README.md
    
            * Update README.md
    
        commit 63b634d48db8644ab7e236a93c6fc859b227038e
        Author: William Wu <74469686+wllmwu@users.noreply.github.com>
        Date:   Fri Mar 10 07:56:24 2023 -0800
    
            Update CODEOWNERS
    
        commit 7a975de437b35662a73cc06718534e168405dd77
        Author: Victor Siu <siuleungjing@gmail.com>
        Date:   Thu Mar 9 12:07:46 2023 -0800
    
            Feature/vs2961/backend category (#22)
    
            * Add csv link + new endpoint for category
    
            * Added visualization service and get viz by title
    
            * renamed git viz by name
    
            * Make category route return actual visualizations, instead of titles
    
            * Refactor codebase to use import + prettier
    
            * Fix bugs + continue refactoring errors
    
            * Fix eslint + address pr review
    
            * Fix prettier check
    
            * Add service layer for visualizations
    
            * Add tier services
    
            * Fix prettier
    
            * Fix bugs
    
            * Change getVisualization to name
    
            ---------
    
            Co-authored-by: James Luo <jaluo@ucsd.edu>
        Co-authored-by: Brenton Dunn <brentonmdunn@users.noreply.github.com>
    
        * Fixed loginslice and subscribeslice Lint errors
    
        * Fixed TableauEmbed Lint errors.
    
        * Removed Unecessary imports from municipal
    
        * Delete .DS_Store
    
        ---------
    
        Co-authored-by: Brenton Dunn <brentonmdunn@users.noreply.github.com>
        Co-authored-by: Victor Siu <siuleungjing@gmail.com>
        Co-authored-by: Brenton Dunn <47510212+brentonmdunn@users.noreply.github.com>
        Co-authored-by: James Luo <jaluo@ucsd.edu>
        Co-authored-by: NirmalAgnihotri <53363382+NirmalAgnihotri@users.noreply.github.com>

--- SDCTA/src/components/Body.tsx
+++ SDCTA/src/components/Body.tsx
@@ -1,8 +1,9 @@
-import landscape from "./../../Images/Landing_Image.jpg";
+import landscape from "../Images/unsplash_dsV4yvL5UJk.jpg";
+// import EdVis from '../Images/Graph.jpg';
 import "./body.css";
-import EducationVis from "./../../Images/Education_Standin.png";
-import HomelessnessVis from "./../../Images/Homelessness_Standin.png";
-import MunicipalVis from "./../../Images/Municipal_Standin.png";
+import EducationVis from "../Images/Education_Standin.png";
+import HomelessnessVis from "../Images/Homelessness_Standin.png";
+import MunicipalVis from "../Images/Municipal_Standin.png";
 
 export const Body: React.FC = () => {
   return (
@@ -16,42 +17,42 @@ export const Body: React.FC = () => {
           magna aliqua. Ut enim ad minim veniam, sed do eiusmod tempor
           incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
         </div>
-        <div className="imgbox">
-          <img className="center-fit" src={landscape} alt="sd landscape" />
-        </div>
+        <img src={landscape} alt="sd landscape" />
       </div>
-      <div className="image_line">
-        <div className="first_Visual">
+      <div className="box">
+        <div className="one">
           <div className="img">
-            <img src={EducationVis} alt="Education Visualization"></img>
+            <a href="../pages/Education.tsx">
+              <img src={EducationVis} alt="Education Visualization"></img>
+            </a>
           </div>
-          <div className="image_line-heading">Education</div>
-          <div className="image_line-description">
+          <div className="box-heading">Education</div>
+          <div className="box-description">
             Lorem ipsum dolor sit amet, sed do consectetur adipiscing elit, sed
             do eiusmod tempor incididunt ut labore et
           </div>
         </div>
-        <div className="second_Visual">
+        <div className="two">
           <div className="img">
             <img src={HomelessnessVis} alt="Homelessness Visualization"></img>
           </div>
-          <div className="image_line-heading">Homelessness</div>
-          <div className="image_line-description">
+          <div className="box-heading">Homelessness</div>
+          <div className="box-description">
             Lorem ipsum dolor sit amet, sed do consectetur adipiscing elit, sed
             do eiusmod tempor incididunt ut labore et
           </div>
         </div>
-        <div className="third_Visual">
+        <div className="three">
           <div className="img">
             <img src={MunicipalVis} alt="Municipal Visualization"></img>
           </div>
-          <div className="image_line-heading">Municipal</div>
-          <div className="image_line-description">
+          <div className="box-heading">Municipal</div>
+          <div className="box-description">
             Lorem ipsum dolor sit amet, sed do consectetur adipiscing elit, sed
             do eiusmod tempor incididunt ut labore et
           </div>
         </div>
-      </div>
+      </div>{" "}
     </>
   );
 };
