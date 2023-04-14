52e00d1b7055dbcc8aef4b54b01335914e403b1f
Tikpai <hrithikp910@gmail.com>
2023-04-14 16:54:18 -0700

    Finished styling the profile and payment info columns

--- SDCTA/src/pages/Profile.tsx
+++ SDCTA/src/pages/Profile.tsx
@@ -15,69 +15,128 @@ export const Profile: React.FC = () => {
   return (
     <div>
       <img className = "header" src={landscape} alt="sd landscape" />
-      <div className = "profile-info">
-        <h2>Profile Information</h2>
-        <form onSubmit={handleSubmit}>
-          <label className = "label">Affiliated Company
-            <input className="long-input"/>
-          </label>
-          <label className = "label">Full Name
-            <input className="long-input"/>
-          </label >
-          <label className = "label">Email Address
+      <div className = "parent_box">
+        <div className = "profile-info">
+          <h2>Profile Information</h2>
+          <form onSubmit={handleSubmit}>
+            <label className = "label">Affiliated Company
               <input className="long-input"/>
-          </label >
-          <label className = "label">Password
-            <input className="long-input"/>
-          </label>
-          <label className = "label">Phone Number
-            <input className="long-input"/>
-          </label>
-          <label className = "label">Mailing Address
-            <input className="long-input"/>
-          </label>
-          <div className = "threeInOne">
-            <div className = "cityDiv">
-              <label className = "label-short">City
-                <input className="short-input"/>
-              </label>
+            </label>
+            <label className = "label">Full Name
+              <input className="long-input"/>
+            </label >
+            <label className = "label">Email Address
+                <input className="long-input"/>
+            </label >
+            <label className = "label">Password
+              <input className="long-input"/>
+            </label>
+            <label className = "label">Phone Number
+              <input className="long-input"/>
+            </label>
+            <label className = "label">Mailing Address
+              <input className="long-input"/>
+            </label>
+            <div className = "threeInOne">
+              <div className = "cityDiv">
+                <label className = "label-short">City
+                  <input className="short-input"/>
+                </label>
+              </div>
+              <div className = "threeBlockDiv">
+                <label className = "label-short">State
+                  <select className="stateDrop">
+                    {stateList.state.map((state) => {
+                      return <option key={state.id} value={state.id}>{state.name}</option>
+                    })}
+                  </select>
+                </label>
+              </div>
+              <div className = "threeBlockDiv">
+                <label className = "label-short">Zip Code
+                  <input className="short-input"/>
+                </label>
+              </div>
             </div>
-            <div className = "threeBlockDiv">
-              <label className = "label-short">State
-                <select className="stateDrop">
-                  {stateList.state.map((state) => {
-                    return <option key={state.id} value={state.id}>{state.name}</option>
-                  })}
+            <label className = "label">Country
+              <select className="long-input">
+                {countryList.country.map((country) => {
+                  return <option key={country.code} value={country.code}>{country.name}</option>
+                })}
                 </select>
-              </label>
-            </div>
-            <div className = "threeBlockDiv">
-              <label className = "label-short">Zip Code
-                <input className="short-input"/>
-              </label>
-            </div>
-          </div>
-          <label className = "label">Country
-            <select className="long-input">
-              {countryList.country.map((country) => {
-                return <option key={country.code} value={country.code}>{country.name}</option>
-              })}
+            </label>
+            <label className = "label">Preferred Language
+              <select className="long-input">
+                {languageList.language.map((language) => {
+                  return <option key={language.code} value={language.code}>{language.name}</option>
+                })}
               </select>
-          </label>
-          <label className = "label">Preferred Language
-            <select className="long-input">
-              {languageList.language.map((language) => {
-                return <option key={language.code} value={language.code}>{language.name}</option>
-              })}
-            </select>
-          </label>
-          <input type="submit" />
-        </form>
-      </div>
-      <div className = "Payment Information">
-        <h2>Payment Information</h2>
+            </label>
+            <input type="submit" />
+          </form>
+        </div>
+        <div className = "payment_info">
+          <h2>Payment Information</h2>
+          <form onSubmit={handleSubmit}>
+            <label className = "label">Card Holder Name
+              <input className="long-input"/>
+            </label>
+            <label className = "label">Full Name
+              <input className="long-input"/>
+            </label >
+            <div className = "threeInOne">
+              <div className = "cardDiv">
+                <label className = "card-label">Card Number
+                  <input className="card-input"/>
+                </label>
+              </div>
+              <div className = "twoDiv">
+                  <label className = "label-short">Exp Date
+                    <input className="shorter-input"/>
+                  </label>
+              </div>
+              <div className = "twoDiv">
+                <label className = "label-short">CVC
+                  <input className="shorter-input"/>
+                </label>
+              </div>
+            </div>
+            <label className = "label">Billing Address
+                <input className="long-input"/>
+            </label >
+            <div className = "threeInOne">
+              <div className = "cityDiv">
+                <label className = "label-short">City
+                  <input className="short-input"/>
+                </label>
+              </div>
+              <div className = "threeBlockDiv">
+                <label className = "label-short">State
+                  <select className="stateDrop">
+                    {stateList.state.map((state) => {
+                      return <option key={state.id} value={state.id}>{state.name}</option>
+                    })}
+                  </select>
+                </label>
+              </div>
+              <div className = "threeBlockDiv">
+                <label className = "label-short">Zip Code
+                  <input className="short-input"/>
+                </label>
+              </div>
+            </div>
+            <label className = "label">Country
+              <select className="long-input">
+                {countryList.country.map((country) => {
+                  return <option key={country.code} value={country.code}>{country.name}</option>
+                })}
+                </select>
+            </label>
 
-      </div>
+            <input type="submit" />
+          </form>
+        </div>
+        </div>
       <Footer />
     </div>
 
