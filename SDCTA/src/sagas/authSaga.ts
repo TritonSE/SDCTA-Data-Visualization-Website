194eb89afb397e555495e536a0385c3c59b0d1bb
Aammya Sapra <79061216+doraemon127@users.noreply.github.com>
2023-04-23 16:43:15 -0700

    Create authSaga.ts

---
+++ SDCTA/src/sagas/authSaga.ts
@@ -0,0 +1,26 @@
+
+import { call, put, takeEvery, takeLatest } from "redux-saga/effects";
+
+
+
+/*
+const fetchUser = () => {};
+
+
+  Starts fetchUser on each dispatched `USER_FETCH_REQUESTED` action.
+  Allows concurrent fetches of user.
+
+function* mySaga() {
+    yield takeEvery("USER_FETCH_REQUESTED", fetchUser);
+  }
+  
+  export default mySaga;
+*/
+
+function* registerSaga() {
+    try {
+        yield
+    } catch (error) {
+
+    }
+}
