3c1a8ce0bfc801977841ecf6201d4a556154b77d
Anthonyp0329 <anthonyp0329@gmail.com>
2023-05-01 14:32:34 -0700

    Update consumer.ts

--- SDCTA/src/api/consumer.ts
+++ SDCTA/src/api/consumer.ts
@@ -1,11 +1,12 @@
 interface userDetails {
-  phoneIn?: string;
-  addressIn?: string;
-  cityIn?: string;
-  stateIn?: string;
-  zipCodeIn?: string;
-  countryIn?: string;
+  phone?: string;
+  address?: string;
+  city?: string;
+  state?: string;
+  zipCode?: string;
+  country?: string;
 }
+
 const updateUserDetails = async (
   emailIn: string,
   userDetails: userDetails
@@ -13,20 +14,17 @@ const updateUserDetails = async (
   const updateRequestOptions = {
     method: "PATCH",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      phone: userDetails.phoneIn,
-      address: userDetails.addressIn,
-      city: userDetails.cityIn,
-      state: userDetails.stateIn,
-      zipCode: userDetails.zipCodeIn,
-      country: userDetails.countryIn,
-    }),
+    body: JSON.stringify(userDetails),
   };
+
   const requestLink = "http://localhost:3001/user/";
+
   const response = await fetch(
     requestLink.concat(emailIn),
     updateRequestOptions
   );
+
   return response;
 };
+
 export { updateUserDetails };
