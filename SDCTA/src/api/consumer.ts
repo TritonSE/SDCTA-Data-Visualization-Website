d7b1f59531f03340fa74b950158fa5a7b4489565
Anthonyp0329 <anthonyp0329@gmail.com>
2023-04-30 17:29:52 -0700

    updates based on change suggestions

--- SDCTA/src/api/consumer.ts
+++ SDCTA/src/api/consumer.ts
@@ -1,44 +1,32 @@
+interface userDetails {
+  phoneIn?: string;
+  addressIn?: string;
+  cityIn?: string;
+  stateIn?: string;
+  zipCodeIn?: string;
+  countryIn?: string;
+}
 const updateUserDetails = async (
-  phoneIn: string,
-  addressIn: string,
-  cityIn: string,
-  stateIn: string,
-  zipCodeIn: string,
-  countryIn: string,
-  emailIn: string
+  emailIn: string,
+  userDetails: userDetails
 ): Promise<Response> => {
-  const idRequestOptions = {
-    method: "GET",
-    headers: { "Content-Type": "application/json" }
+  const updateRequestOptions = {
+    method: "PATCH",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      phone: userDetails.phoneIn,
+      address: userDetails.addressIn,
+      city: userDetails.cityIn,
+      state: userDetails.stateIn,
+      zipCode: userDetails.zipCodeIn,
+      country: userDetails.countryIn
+    }),
   }
-  let response = await fetch(
-    "http://localhost:3001/user/" + emailIn,
-    idRequestOptions
-  )
-  response.json().then(async (result) => {
-    const updateRequestOptions = {
-      method: "PATCH",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        phone: phoneIn,
-        address: addressIn,
-        city: cityIn,
-        state: stateIn,
-        zipCode: zipCodeIn,
-        country: countryIn
-      }),
-    }
-    const id = result._id;
-    const requestLink = "http://localhost:3001/user/"
-    response = await fetch(
-      requestLink.concat(id),
-      updateRequestOptions
-    );
-    return response;
-  }).catch((error) => {
-    return error;
-  })
+  const requestLink = "http://localhost:3001/user/"
+  const response = await fetch(
+    requestLink.concat(emailIn),
+    updateRequestOptions
+  );
   return response;
-  // return response;
 }
 export { updateUserDetails };
---
+++ server/services/user.js
@@ -0,0 +1,43 @@
+import Model from "../models/user.js";
+import { ServiceError } from "../errors.js";
+import { getTierByLevel } from "./tier.js";
+
+export async function getUserByEmail(email) {
+  const user = await Model.findOne({ email });
+  if (!user) {
+    throw ServiceError.USER_NOT_FOUND;
+  }
+  return user;
+}
+
+export async function createUser(body) {
+  try {
+    console.log(body);
+    const tier = await getTierByLevel(body.tierLevel);
+    const data = new Model(body);
+    data.tier = tier;
+    return await data.save();
+  } catch (error) {
+    throw ServiceError.INVALID_USER_RECEIVED.addContext(error);
+  }
+}
+
+export async function updateUser(email, body) {
+  try {
+    const options = { new: true, returnNewDocument: true };
+    if (body.tierLevel) {
+      await getTierByLevel(body.tierLevel);
+    }
+    return await Model.findOneAndUpdate({ email }, body, options);
+  } catch (error) {
+    throw ServiceError.INVALID_USER_RECEIVED.addContext(error);
+  }
+}
+
+export async function deleteUser(id) {
+  try {
+    return await Model.findByIdAndDelete(id);
+  } catch (error) {
+    throw ServiceError.INVALID_USER_RECEIVED.addContext(error);
+  }
+}
