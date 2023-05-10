a9539037ce83430d5931835cbd8a38e8f0e02767
Anthonyp0329 <anthonyp0329@gmail.com>
2023-05-10 12:08:07 -0700

    storing user in redux state

--- SDCTA/src/App.tsx
+++ SDCTA/src/App.tsx
@@ -1,9 +1,8 @@
 import "./App.css";
 import { Routes } from "./components/Routes";
 import { auth } from "./firebase-config";
-import { login, logout, storeUser } from "./slices/loginSlice";
+import { logout } from "./slices/loginSlice";
 import { useDispatch } from "react-redux";
-import { getUser } from "./api/auth"
 
 declare global {
   interface Window {
@@ -15,10 +14,9 @@ const App: React.FC = () => {
   const dispatch = useDispatch();
   auth.onAuthStateChanged(async (user) => {
     if (user != null) {
-      dispatch(login());
-      console.log(user.email);
-      await getUser(user.email).then((mongoUser) => {
-        dispatch(storeUser(mongoUser));
+      dispatch({
+        type: 'STORE_USER',
+        payload: user.email
       });
     } else {
       dispatch(logout());
--- SDCTA/src/sagas/authSaga.ts
+++ SDCTA/src/sagas/authSaga.ts
@@ -1,6 +1,6 @@
 import { call, put, takeEvery } from "redux-saga/effects";
-import { loginUser } from "../api/auth"
-import { login } from "../slices/loginSlice"
+import { loginUser, getUser } from "../api/auth"
+import { login, storeUser } from "../slices/loginSlice"
 /*
 const fetchUser = () => {};
 
@@ -13,10 +13,20 @@ function* mySaga() {
 
   export default mySaga;
 */
+
+function * setUser ({ payload }: any): Generator<any> {
+  try {
+    const user = yield call(getUser, payload);
+
+    yield put(storeUser(user));
+    yield put(login());
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 function * authenticateUser ({ payload }: any): Generator<any> {
   try {
-    console.log("auth");
-    console.log(payload);
     yield call(loginUser, payload.loginPassword, payload.rememberUser, payload.loginEmail);
 
     yield put(login());
@@ -27,6 +37,7 @@ function * authenticateUser ({ payload }: any): Generator<any> {
 
 function * registerSaga (): Generator<any> {
   yield takeEvery("LOGIN_USER", authenticateUser);
+  yield takeEvery("STORE_USER", setUser);
 }
 
 export default registerSaga;
