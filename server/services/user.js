d20d50333fd89a7b6c70af5381132ddffdfb29f0
Anthonyp0329 <anthonyp0329@gmail.com>
2023-05-17 15:52:52 -0700

    adding card/payment methods

--- server/services/user.js
+++ server/services/user.js
@@ -1,21 +1,45 @@
-import Model from "../models/user.js";
+import UserModel from "../models/user.js";
 import { ServiceError } from "../errors.js";
 import { getTierByLevel } from "./tier.js";
+import stripeSetup from "stripe";
+import dotenv from "dotenv";
+dotenv.config();
+
+const stripe = stripeSetup(process.env.STRIPE_SECRET_KEY);
 
 export async function getUserByEmail(email) {
-  const user = await Model.findOne({ email });
+  const user = await UserModel.findOne({ email });
   if (!user) {
     throw ServiceError.USER_NOT_FOUND;
   }
   return user;
 }
 
+async function createStripeUser(name, email) {
+  try {
+    return await stripe.customers.create({
+      name,
+      email
+    });
+  } catch (error) {
+    console.log(error);
+    throw ServiceError.STRIPE_FAILURE.addContext(error);
+  }
+}
+
 export async function createUser(body) {
   try {
     console.log(body);
+    const fullName = `${body.firstName} ${body.lastName}`;
+
+    const customer = await createStripeUser(fullName, body.email);
+
+    console.log(body);
     const tier = await getTierByLevel(body.tierLevel);
-    const data = new Model(body);
+    const data = new UserModel(body);
+    data.stripe_id = customer.id;
     data.tier = tier;
+
     return await data.save();
   } catch (error) {
     throw ServiceError.INVALID_USER_RECEIVED.addContext(error);
@@ -25,7 +49,7 @@ export async function createUser(body) {
 export async function updateUser(email, body) {
   try {
     const options = { new: true, returnNewDocument: true };
-    const data = await Model.findOneAndUpdate({ email }, body, options);
+    const data = await UserModel.findOneAndUpdate({ email }, body, options);
     if (body.tierLevel) {
       const tier = await getTierByLevel(body.tierLevel);
       data.tier = tier;
@@ -38,8 +62,28 @@ export async function updateUser(email, body) {
 
 export async function deleteUser(id) {
   try {
-    return await Model.findByIdAndDelete(id);
+    return await UserModel.findByIdAndDelete(id);
   } catch (error) {
     throw ServiceError.INVALID_USER_RECEIVED.addContext(error);
+  }
+}
+
+export async function addStripeCard(body) {
+  try {
+    const paymentMethod = await stripe.paymentMethods.create({
+      type: "card",
+      card: {
+        number: body.number,
+        exp_month: body.exp_month,
+        exp_year: body.exp_year,
+        cvc: body.cvc
+      }
+    });
+
+    const currentUser = await getUserByEmail(body.email);
+
+    return await stripe.paymentMethods.attach(paymentMethod.id, { customer: currentUser.stripe_id });
+  } catch (error) {
+    throw ServiceError.INVALID_CARD_RECIEVED.addContext(error);
   }
 }
