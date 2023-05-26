75a39124b39648415e5fbeb1feac0a73ba8ff5da
Helen <59126064+Miyuki-L@users.noreply.github.com>
2023-05-25 20:01:15 -0700

    Changed fallback value for API_URL

--- SDCTA/src/api/consumer.ts
+++ SDCTA/src/api/consumer.ts
@@ -1,6 +1,6 @@
 import { type VisualizationObject } from "./data";
-export const API_URL = process.env.REACT_APP_API_URL;
-export const GET_VIS = `${API_URL ?? "http://localhost:3001"}/visualization`;
+export const API_URL = process.env.REACT_APP_API_URL ?? "";
+export const GET_VIS = `${API_URL}/visualization`;
 
 export const getVisByTitle = async (
   title: string
