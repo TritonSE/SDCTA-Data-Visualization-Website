fa3efdaa8757b672e2e50f5e1028798a8c60c5a7
NirmalAgnihotri <53363382+NirmalAgnihotri@users.noreply.github.com>
2023-06-16 01:22:02 -0700

    prettier run

--- SDCTA/src/components/ErrorBoundary.jsx
+++ SDCTA/src/components/ErrorBoundary.jsx
@@ -1,23 +1,23 @@
-import React from 'react'
-import { Error404 } from '../components/404'
+import React from "react";
+import { Error404 } from "../components/404";
 
 class ErrorBoundary extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = { error: false }
+  constructor(props) {
+    super(props);
+    this.state = { error: false };
   }
 
-  static getDerivedStateFromError (error) {
-    return { error: error }
+  static getDerivedStateFromError(error) {
+    return { error: error };
   }
 
-  render () {
+  render() {
     if (this.state.error) {
-      return <Error404 />
+      return <Error404 />;
     }
 
-    return this.props.children
+    return this.props.children;
   }
 }
 
-export default ErrorBoundary
+export default ErrorBoundary;
