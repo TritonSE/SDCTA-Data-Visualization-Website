92914840d94fa9e5108286fde99c653c0834f291
NirmalAgnihotri <53363382+NirmalAgnihotri@users.noreply.github.com>
2023-07-06 20:41:04 -0700

    Removed process log

--- client/src/firebase-setup.js
+++ client/src/firebase-setup.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-console.log(process.env);
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
